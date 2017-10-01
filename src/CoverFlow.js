ce059f37a5f243ff40abe11b6e004b5886b0e1e6
Jonathan Brandel <jono@with.in>
2017-09-30 18:01:08 -0700

    added Browser Bar

--- src/CoverFlow.js
+++ src/CoverFlow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Promise from 'promise';
 import {
   WebGLRenderer, Scene, PerspectiveCamera, Raycaster, Vector2, Vector3,
   PlaneBufferGeometry, Fog, Mesh, MeshBasicMaterial, Texture
@@ -11,6 +12,7 @@ class CoverFlow extends Component {
     super(props);
     this.state = {};
     this.drag = 0.0625;
+    this.BrowserHeight = 20;
   }
   shouldComponentUpdate() {
     return false;
@@ -96,49 +98,100 @@ class CoverFlow extends Component {
 
     var scope = this;
     var canvas = document.createElement('canvas');
-    canvas.width = canvas.height = 1024;
+    var ctx = canvas.getContext('2d');
 
-    var mesh = new Mesh(
-      this.geometry, new MeshBasicMaterial({
-        color: '#efefef'
-      })
-    );
+    canvas.width = canvas.height = 1024;
 
     var image = document.createElement('img');
     image.crossOrigin = 'anonymous';
-    image.onload = function() {
 
-      var sx = 0;
-      var sy = 0;
-      var sw = image.width;
-      var sh = image.width * scope.height / scope.width;
+    var mesh = new Mesh(
+      this.geometry, new MeshBasicMaterial({
+        color: '#efefef',
+        transparent: true,
+        opacity: 0
+      })
+    );
+    mesh.userData.position = new Vector3();
+    mesh.userData.opacity = 0;
+    mesh.userData.promise = new Promise(function(resolve, reject) {
 
-      var dx = 0;
-      var dy = 0;
-      var dw = canvas.width;
-      var dh = canvas.height;
+      image.onload = function(e) {
 
-      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-      canvas.getContext('2d').drawImage(
-        image, sx, sy, sw, sh, dx, dy, dw, dh);
+        var sx = 0;
+        var sy = 0;
+        var sw = image.width;
+        var sh = image.width * scope.height / scope.width;
 
-      mesh.material.color.set('white');
-      mesh.material.map = new Texture(canvas);
+        var dx = 0;
+        var dy = scope.BrowserHeight;
+        var dw = canvas.width;
+        var dh = canvas.height;
 
-      mesh.material.map.needsUpdate = true;
-      mesh.material.needsUpdate = true;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
 
-    };
-    image.src = data.screenshot_url;
-    console.log(image.src);
+        scope.drawBrowserBar(canvas, ctx, data);
 
-    mesh.position.z = - this.distance * i;
+        mesh.material.color.set('white');
+        mesh.material.map = new Texture(canvas);
+        mesh.material.map.anisotropy = scope.renderer.getMaxAnisotropy();
+
+        mesh.material.map.needsUpdate = true;
+        mesh.material.needsUpdate = true;
+        resolve(e);
+
+      };
+      image.onerror = reject;
+      image.src = data.screenshot_url;
+
+    });
+
+    mesh.position.z = mesh.userData.position.z = - this.distance * i;
+    mesh.position.y = mesh.userData.position.y = - this.camera.position.y;
     mesh.rotation.x = - Math.PI / 10;
     mesh.scale.x = this.width;
     mesh.scale.y = this.height;
     mesh.userData.model = data;
 
     return mesh;
+
+  }
+  drawBrowserBar(canvas, ctx, data) {
+
+    var w = canvas.width;
+    var h = this.BrowserHeight;
+    var r = h / 4;
+    var fontSize = h * 0.85;
+
+    ctx.fillStyle = '#ccc';
+    ctx.beginPath();
+    ctx.moveTo(0, h);
+    ctx.lineTo(0, h / 2);
+    ctx.quadraticCurveTo(0, 0, h / 2, 0);
+    ctx.lineTo(w - h / 2, 0);
+    ctx.quadraticCurveTo(w, 0, w, h / 2);
+    ctx.lineTo(w, h);
+    ctx.closePath();
+
+    ctx.fill();
+
+    ctx.fillStyle = '#efefef';
+
+    ctx.beginPath();
+    for (var i = 0; i < 3; i++) {
+      var x = h * (i + 1);
+      var y = h / 2;
+      ctx.moveTo(x, y);
+      ctx.arc(x, y, r, 0, Math.PI * 2);
+    }
+    ctx.fill();
+
+    ctx.fillStyle = '#333';
+    ctx.font = '900 ' + fontSize + 'px/' + fontSize + 'px Arial, sans-serif';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(data.timestamp.slice(0, 4).split('').join(' '), w / 2, h / 2);
 
   }
   click(e) {
@@ -155,8 +208,6 @@ class CoverFlow extends Component {
 
     if (intersects.length > 0) {
       mesh = intersects[0].object;
-      // TODO: Use mesh.userData and the real data to
-      // pop open the right URL
       window.open(mesh.userData.model.url, '_blank');
     }
 
@@ -179,6 +230,21 @@ class CoverFlow extends Component {
         .multiplyScalar(this.drag)
     );
 
+    for (var i = 0; i < this.scene.children.length; i++) {
+      var mesh = this.scene.children[i];
+      if (mesh.userData.position !== undefined) {
+        mesh.position.add(
+          v3.copy(mesh.userData.position)
+            .sub(mesh.position)
+            .multiplyScalar(this.drag * 2)
+        );
+      }
+      if (mesh.userData.opacity !== undefined) {
+        mesh.material.opacity += (mesh.userData.opacity
+          - mesh.material.opacity) * this.drag * 2;
+      }
+    }
+
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(this._loop);
 
@@ -188,8 +254,8 @@ class CoverFlow extends Component {
   }
   componentWillReceiveProps(nextProps) {
 
+    var scope = this;
     var props = nextProps;
-    console.log(props);
 
     if (this.initiated) {
 
@@ -210,12 +276,19 @@ class CoverFlow extends Component {
         this.range.min = Math.min(this.range.min, mesh.position.z);
         this.range.max = Math.max(this.range.max, mesh.position.z);
 
-        this.scene.add(mesh);
+        mesh.userData.promise.then(function() {
+          mesh.userData.position.y = 0;
+          mesh.userData.opacity = 1;
+          scope.scene.add(mesh);
+        });
 
       }
 
-      this.range.min += this.distance * 0.66;
-      this.range.max += this.distance * 0.66;
+      if (props.data.length > 0) {
+        this.range.min += this.distance * 0.66;
+        this.range.max += this.distance * 0.66;
+        this.camera.userData.position.z = this.range.max;
+      }
 
     }
 
